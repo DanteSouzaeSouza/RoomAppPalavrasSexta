b9ae0815f9633bed3e4794f066c6e3b27ac694c9
Dante Souza e Souza <lokinmodar@gmail.com>
2020-05-29 20:58:03 -0300

    Added Entity

---
+++ app/src/main/java/br/com/theoldpinkeye/roomapppalavrassexta/model/Word.java
@@ -0,0 +1,29 @@
+package br.com.theoldpinkeye.roomapppalavrassexta.model;
+
+// Classe entidade
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+// transformando a classe em Entity no banco de dados do Room
+@Entity(tableName = "word_table") // cria tabela no banco
+public class Word {
+
+  // informando as propriedades do campo na tabela
+  @PrimaryKey // chave primária
+  @NonNull // não aceita valores nulos
+  @ColumnInfo(name = "word") // informando o nome da coluna que vai receber esse dado
+  private String mWord;
+
+  // método construtor
+  public Word(String mWord) {
+    this.mWord = mWord;
+  }
+
+  // método get
+  public String getWord() {
+    return mWord;
+  }
+}
