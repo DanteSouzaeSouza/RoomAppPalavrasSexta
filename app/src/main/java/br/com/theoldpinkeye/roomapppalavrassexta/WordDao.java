4fb7d59162024dd90cc928d9ddb2535b0f645348
Dante Souza e Souza <lokinmodar@gmail.com>
2020-05-29 21:09:57 -0300

    Added DAO Interface

---
+++ app/src/main/java/br/com/theoldpinkeye/roomapppalavrassexta/WordDao.java
@@ -0,0 +1,26 @@
+package br.com.theoldpinkeye.roomapppalavrassexta;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import br.com.theoldpinkeye.roomapppalavrassexta.model.Word;
+import java.util.List;
+// transformando a Interface em um DAO
+
+@Dao
+public interface WordDao {
+
+  // permitir a inserção de uma palavra repetida
+  // passando orientação para igonar o erro
+  @Insert(onConflict = OnConflictStrategy.IGNORE)
+  void insert(Word word);
+
+  // Query que deleta os dados
+  @Query("DELETE FROM word_table")
+  void deleteAll();
+
+  // Seleciona os dados em ordem alfabética
+  @Query("SELECT * FROM word_table ORDER BY word ASC")
+  List<Word> getAlphabetizedWords();
+}
